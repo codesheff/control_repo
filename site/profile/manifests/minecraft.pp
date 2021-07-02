e17dd6e92b5145abc5f9795a4ff2590a03d71ae9
codesheff <codesheff@outlook.com>
2021-07-02 16:48:24 +0100

    Update minecraft.pp

--- site/profile/manifests/minecraft.pp
+++ site/profile/manifests/minecraft.pp
@@ -1,4 +1,8 @@
 class profile::minecraft {
-  include minecraft
+    ## include minecraft
+    file {'/root/README':
+    ensure => file,
+    content => "Welcome to ${fqdn}. This is the minecraft server",
+    owner => 'root',
 }
 
