1cfb7e91191a6c805d83c6c9064526f39b581a2e
codesheff <codeshef@outlook.com>
2021-07-02 15:31:00 +0100

    04_07_again

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,7 +2,6 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node { 'web.puppet.vm': }
   dockeragent::node { 'db.puppet.vm': }
-<<<<<<< HEAD
   host {‘web.puppet.vm’:
     ensure => present,
     ip => ‘172.18.0.2’,
@@ -10,14 +9,5 @@ class profile::agent_nodes {
   host {‘db.puppet.vm’:
     ensure => present,
     ip => ‘172.18.0.3’,
-=======
-  host {'web.puppet.vm':
-    ensure => present, 
-    ip     => '172.18.0.2'
-  }
-  host {'db.puppet.vm':
-    ensure => present, 
-    ip     => '172.18.0.3'
->>>>>>> 75c8b59d6297166bab18bb4b773ac7147b8e8fe2
   }
 }
